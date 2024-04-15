535f74697f41b2ca76b942a05b724b7b48fec1a4
Carlos Henrique Alves <carloshadp@gmail.com>
2024-04-15 16:49:37 -0300

    colocando no github

---
+++ Training&Functions/calculator.py
@@ -0,0 +1,24 @@
+#CALCULATOR 
+import math 
+
+
+
+
+
+def calculate(x,y): 
+    pass
+def multiply(x,y):
+    print(f'You want to multiply{x} and {y}, here is your solution')
+    
+def divide(x,y):
+    print(f'You want to divide{x} and {y}, here is your solution')
+    
+def add(x,y):
+    print(f'You want to add{x} and {y}, here is your solution')
+    
+def sub(x,y):
+    print(f'You want to subtract{x} and {y}, here is your solution')
+    sub = x - y
+    print(sub)
+    
+sub(10,30)
---
+++ Training&Functions/messages.py
@@ -0,0 +1,5 @@
+def hello():
+    print('Hello have a good day!')
+
+def bye():
+    print('Have a good one!')
